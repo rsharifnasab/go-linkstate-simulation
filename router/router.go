22fddfa066a94c92bd8c43f2c7b437ff04bd90f6
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-06-25 21:17:17 +0430

    messed up

--- router/router.go
+++ router/router.go
@@ -8,44 +8,73 @@ import (
 	"os"
 )
 
-type x struct {
-	routerCount int
+type Router struct {
+	conn    *net.UDPConn
+	Port    int
+	logFile *os.File
+
+	RouterCount int
+	Neighbors   []*Edge
 }
 
-func main() {
-	udpConn, err, port := startUDPServer()
-	pnc(err)
-	_ = udpConn
-
-	logFile := initloger(port)
-	defer logFile.Close()
-
-	manager := NewManager("localhost:8585")
-	defer manager.conn.Close()
-
-	manager.write(port)
-
-	// todo
-	readConnTable()
-
+type Edge struct {
+	Dist int
+	Cost int
 }
 
-func readConnTable() {
-	log.Printf("read conn table")
-}
-
-func initloger(port int) *os.File {
-	logFileAdd := fmt.Sprintf("../%v.log", port)
+func (router *Router) InitLogger() {
+	logFileAdd := fmt.Sprintf("../%v.log", router.Port)
 	logFile, err := os.OpenFile(logFileAdd, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	pnc(err)
+
+	router.logFile = logFile
 
 	log.SetOutput(logFile)
 	log.SetFlags(0)
 	log.Printf("")
 	//log.Printf("- - - %v logger - - -", port)
-	log.SetPrefix(fmt.Sprintf("child %v ", port))
+	log.SetPrefix(fmt.Sprintf("child %v ", router.Port))
 
-	return logFile
+}
+
+func (router *Router) StartUDPServer() {
+	const MAX_TRIES = 3
+	var err error
+	for failures := 0; failures < MAX_TRIES; failures++ {
+		port := getSomeFreePort()
+		addr := net.UDPAddr{
+			Port: port,
+			IP:   net.ParseIP("127.0.0.1"),
+		}
+		conn, err := net.ListenUDP("udp", &addr)
+		if err == nil {
+			router.conn = conn
+			router.Port = port
+		}
+	}
+	panic(err)
+}
+
+func main() {
+	router := &Router{}
+
+	router.StartUDPServer()
+
+	router.InitLogger()
+	defer router.logFile.Close()
+
+	router.ConnectToManager("localhost:8585")
+	defer router.manager.conn.Close()
+
+	manager.write(port)
+
+	// todo
+	newRouter(manger)
+
+}
+
+func readConnTable() {
+	log.Printf("read conn table")
 }
 
 func getSomeFreePort() int {
@@ -54,22 +83,6 @@ func getSomeFreePort() int {
 	//fmt.Fprintf(os.Stderr, "using port: %+v\n", listener.Addr().(*net.TCPAddr))
 	pnc(listener.Close())
 	return listener.Addr().(*net.TCPAddr).Port
-}
-
-func startUDPServer() (*net.UDPConn, error, int) {
-	var err error
-	for failures := 0; failures < 3; failures++ {
-		port := getSomeFreePort()
-		addr := net.UDPAddr{
-			Port: port,
-			IP:   net.ParseIP("127.0.0.1"),
-		}
-		conn, err := net.ListenUDP("udp", &addr)
-		if err == nil {
-			return conn, nil, port
-		}
-	}
-	return nil, err, -1
 }
 
 // unused by now
